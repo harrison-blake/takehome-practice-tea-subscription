40e00c3e67a71191b4d03f7fdfae108d3aa96fce
Harrison Blake <harrison.r.blake.gmail.com>
2021-07-28 10:13:45 -0600

    Fix strucutre of subscrive customer test

--- spec/requests/api/v1/customers/subscribe_customer_spec.rb
+++ spec/requests/api/v1/customers/subscribe_customer_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 describe 'Subscribe Customer Endpoint' do
+  before :each do
+    @customer = Customer.create!(first_name: 'Harrison', last_name: 'Blake', email: 'test')
+    @test_sub = Subscription.create!(title: 'Summer Mellow', price: 4.99, status: false)
+    @test_sub.teas.create!(title: 'Green Tea', description: 'Green tea is a type of tea that is made from Camellia sinensis leaves and buds that have not undergone the same withering and oxidation process used to make oolong teas and black teas.', brew_time: 10.30)
+    @test_sub.teas.create!(title: 'Mint Tea', description: 'Mint tea is an herbal tea that is appreciated around the world for its fresh aroma and soothing taste. Mint tea benefits are widely promoted, but not all of them are supported by scientific studies. Other types of mint tea—such as Skinny Mint Tea—have become popular based, in part, on the benefits of mint tea.', brew_time: 10.30)
+  end
   describe 'Happy Path' do
-    before :each do
-      @customer = Customer.create!(first_name: 'Harrison', last_name: 'Blake', email: 'test')
-      @test_sub = Subscription.create!(title: 'Summer Mellow', price: 4.99, status: false)
-      @test_sub.teas.create!(title: 'Green Tea', description: 'Green tea is a type of tea that is made from Camellia sinensis leaves and buds that have not undergone the same withering and oxidation process used to make oolong teas and black teas.', brew_time: 10.30)
-      @test_sub.teas.create!(title: 'Mint Tea', description: 'Mint tea is an herbal tea that is appreciated around the world for its fresh aroma and soothing taste. Mint tea benefits are widely promoted, but not all of them are supported by scientific studies. Other types of mint tea—such as Skinny Mint Tea—have become popular based, in part, on the benefits of mint tea.', brew_time: 10.30)
-    end
-
     it "subscribes a customer to a tea subscription" do
       params = {
                 customer_id: @customer.id,
@@ -39,11 +38,30 @@ describe 'Subscribe Customer Endpoint' do
       expect(customer[:data][:attributes][:subscriptions][0][:price]).to eq(4.99)
       expect(customer[:data][:attributes][:subscriptions][0][:status]).to eq("true")
     end
-
+  end
+  describe "sad path" do
     it "returns an error message if the subscription id does not exist" do
       params = {
                 customer_id: @customer.id,
                 subscription_id: 1
+               }
+
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      VCR.use_cassette('subscribe_customer') do
+        post '/api/v1/customers', headers: headers, params: params.to_json
+      end
+
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error).to have_key(:error)
+      expect(error[:error]).to eq("id does not exist")
+    end
+
+    it "returns an error message if the customer id does not exist" do
+      params = {
+                customer_id: @1,
+                subscription_id: @test_sub
                }
 
       headers = {"CONTENT_TYPE" => "application/json"}
